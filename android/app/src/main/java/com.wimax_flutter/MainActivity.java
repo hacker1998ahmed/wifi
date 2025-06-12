fa9ede94172a6fb83625270ab422c10dfa077733
ahmed mostafa <gogom8870@gmail.com>
2025-06-12 18:13:51 +0300

    Delete android directory

--- android/app/src/main/java/com.wimax_flutter/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.wimax_flutter;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // يمكنك إضافة أي تعديلات Native هنا إن احتجت إليها مستقبلاً
-}
